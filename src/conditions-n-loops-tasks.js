340eb3108a6bfe4b5cd3466f07a7ab9ca164c8c8
Nikita Korp <betaquarius203@gmail.com>
2024-12-06 08:40:21 +0300

    feat: implement convertToRomanNumerals function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -108,8 +108,32 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+function convertToRomanNumerals(num) {
+  const roman = {
+    1: 'I',
+    2: 'II',
+    3: 'III',
+    4: 'IV',
+    5: 'V',
+    6: 'VI',
+    7: 'VII',
+    8: 'VIII',
+    9: 'IX',
+    10: 'X',
+  };
+
+  if (num <= 10) return roman[num];
+
+  let romanNumerals = '';
+  const firstDigit = Math.trunc(num / 10);
+
+  for (let i = 1; i <= firstDigit; i += 1) {
+    romanNumerals += roman['10'];
+  }
+
+  if (num % 10 !== 0) romanNumerals += roman[num % 10];
+
+  return romanNumerals;
 }
 
 /**
